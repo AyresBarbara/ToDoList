1e2fe1d346934cb5ae3e86fc858f8252d5224ff4
AyresBarbara <ayresbarbara2@gmail.com>
2025-09-18 15:24:22 -0300

    Construção de tela em React

--- README.md
+++
@@ -1,2 +0,0 @@
-# ToDoList
-O projeto de lista de tarefas (To Do List) desenvolvido com #ReactJS para auxiliar na organização de suas atividades diárias. 
---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [todos, setTodos] = useState([//Permite renderização da minha variavel
+  { //DADOS
+    id:1,
+    text:"Criar funcionalidade X no sistema",
+    category: "Trabalho",
+    isCompleted: false,
+  },
+  {
+    id:2,
+    text:"Ir para a academia",
+    category: "Pessoal",
+    isCompleted: false,
+  },
+  {
+    id:3,
+    text:"Estudar React",
+    category: "Faculdade",
+    isCompleted: false,
+  },
+  {
+    id:4,
+    text:"Aniversário de Gabi",
+    category: "Lazer",
+    isCompleted: false,
+  },
+]);
+  return( <div className='app'> 
+  <h1>Lista de Tarefas</h1> 
+  
+  <div className='todo-list'>
+    {todos.map((todo) => ( //listar meus objetos
+      <div className="todo" key={todo.id}>
+        <div className="content">
+          <p>{todo.text}</p>
+      <p className="category">({todo.category})</p>
+        </div>
+        <div>
+          <button>Completar</button>
+          <button>X</button>
+        </div>
+      </div>
+    ))}
+  </div>
+  </div>
+);
+}
+
+export default App
